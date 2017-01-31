6fbe989f6181ded4df2c48d459e81bace4ceaf41
codejunkienick <nickdeveloper@protonmail.com>
2017-01-31 08:36:41 +0500

    add routes example

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,4 +1,24 @@
-//Export collection of Routes
-const ROUTES = [];
+// Export collection of Routes
+// I don't really know if I want to have a centralized route config structure
+// But if you want to implement it this is an example. 
+// BUT it is not used anywhere in the code 
+// For a much more complete extample visit: https://github.com/ReactTraining/react-router/blob/v4/packages/react-router-website/modules/examples/RouteConfig.js
+const App ({children}) => <div>{children}</div>
+const ROUTES = [
+  {
+    path: '/',
+    component: App,
+    routes: [
+      {
+        path: '/home',
+        component: () => <h2>Home</h2>,
+      },
+      {
+        path: '/contacts',
+        component: () => <h2>Contacts</h2>,
+      },
+    ]
+  }
+];
 
 export default ROUTES;
